6d197ea1e82cdce34c1f34d5cfc4f77c0937d4f2
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-01-05 10:00:21 +0100

    start creating Home page

--- src/container/Home.js
+++ src/container/Home.js
@@ -3,10 +3,42 @@ import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
+import { Sidebar, UserProfile } from '../components';
+import { userQuery } from '../utils/data';
+import { client } from '../client';
+import Pins from './Pins';
+import logo from '../assets/logo.png';
+
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState();
+
+  const userInfo = localStorage.get('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
   return (
-    <div>
-      
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-20" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }

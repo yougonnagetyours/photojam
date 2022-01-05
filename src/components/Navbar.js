a91df0a240dd5a7626efa9f61b7f9a4afee38e08
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-01-05 14:13:36 +0100

    begin setting Navbar and fix route bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { IoMdAdd, IoMdSearch } from 'react-icons/io';
 
-const Navbar = () => {
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   return (
     <div>
       Navbar

c83878dfd92babae4212e1e407dbbc6578109fe6
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-01-05 13:20:26 +0100

    Add few components

--- src/container/Pins.js
+++ src/container/Pins.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components';
 
 const Pins = () => {
   return (
